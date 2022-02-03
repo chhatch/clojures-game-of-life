7802d866b3ee57de33185dc25d91cfee6e80d0db
Chris Hatch <chris.hatch@hotelengine.com>
2022-02-03 08:30:28 -0600

    Click to flip cell color
    Had to refactor board render to map-indexed for performance reasons.
    Components should be passed what they need. How will this work for the
    board update? It's time to split things into separate files.

--- src/app/game_interface.cljs
+++ src/app/game_interface.cljs
@@ -10,20 +10,27 @@
 (defn flip-cell [y x]
   (map-indexed (fn [y2 col] (map-indexed (fn [x2 cell-value] (if (and (= y y2) (= x x2)) (not cell-value) cell-value)) col)) @board-state))
 
-(defn cell-onclick [y x]  
-  (fn [] (println "row-" y " col-" x " value: " (nth (nth @board-state y) x))
-    (update-board (flip-cell y x))))
+(defn cell-onclick [y x cell-value]
+  (fn [] (update-board (flip-cell y x))))
 
-(defn renderCell [y x]
-  [:div {:style {:background-color "yellow" :height "10px" :width "10px" :cursor "pointer"} :on-click (cell-onclick y x)}])
+(defn select-cell-color [cell-value]
+  (if cell-value "white" "yellow"))
 
-(defn renderRow [y]
-  [:div (for [x (range board-size)] ^{:key (str "col-" y "-" x)} [renderCell y x])])
+(defn cell-style [cell-value]
+  {:background-color (select-cell-color cell-value) :height "10px" :width "10px" :cursor "pointer"})
+
+(defn renderCell [y]
+  (fn [x cell-value] ^{:key x}
+    [:div {:style (cell-style cell-value)
+           :on-click (cell-onclick y x cell-value)}]))
+
+(defn renderRow [y row]
+  [:div {:key y} (map-indexed (renderCell y) row)])
 
 (defn renderBoard []
   [:div {:style {:display "flex"}}
-   (for [y (range board-size)]
-     ^{:key (str "row-" y)} [renderRow y])])
+   (map-indexed renderRow @board-state)])
 
 (defn game-interface []
-  [:div {:style {:display "flex" :justify-content "center"}} [:div {:style {:display "flex" :flex-direction "column" :border "solid"}}[renderBoard]]])
+  [:div {:style {:display "flex" :justify-content "center"}}
+   [:div {:style {:display "flex" :flex-direction "column" :border "solid"}} [renderBoard]]])
